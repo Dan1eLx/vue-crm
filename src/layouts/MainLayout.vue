e82309af2f482e4c84d58eca6957291574b97386
Dan1eLx <d4n1elx3@gmail.com>
2020-02-17 00:30:11 +0500

    ninth lesson

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,19 +1,20 @@
 <template>
-    <div class="app-main-layout">
-        <Navbar @click="isOpen = !isOpen" />
-        <Sidebar v-model="isOpen"/>
-        <main class="app-content" :class="{full: !isOpen}">
-        <div class="app-page">
-            <router-view />
-            </div>
-        </main>
+        <Loader v-if="loading" />
+        <div class="app-main-layout" v-else>
+            <Navbar @click="isOpen = !isOpen" />
+            <Sidebar v-model="isOpen"/>
+            <main class="app-content" :class="{full: !isOpen}">
+            <div class="app-page">
+                <router-view />
+                </div>
+            </main>
 
-        <div class="fixed-action-btn">
-            <router-link class="btn-floating btn-large blue" to="/record">
-                <i class="large material-icons">add</i>
-            </router-link>
+            <div class="fixed-action-btn">
+                <router-link class="btn-floating btn-large blue" to="/record">
+                    <i class="large material-icons">add</i>
+                </router-link>
+            </div>
         </div>
-    </div>
 </template>
 
 <script>
@@ -23,8 +24,16 @@ import Sidebar from '@/components/app/Sidebar.vue'
 export default {
     name: 'main-layout',
     data: () => ({
-        isOpen: true
+        isOpen: true,
+        loading: true
     }),
+    async mounted() {
+        if (!Object.keys(this.$store.getters.info).length) {
+            await this.$store.dispatch('fetchInfo')
+        }
+
+        this.loading = false
+    },
     components:{
         Navbar, Sidebar
     }
