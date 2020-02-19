ea951f15d3b03fc5c22418a0d4eb78f84389444a
Dan1eLx <d4n1elx3@gmail.com>
2020-02-20 01:20:50 +0500

    twelfth lesson

--- src/components/CategoryEdit.vue
+++ src/components/CategoryEdit.vue
@@ -5,27 +5,47 @@
             <h4>Редактировать</h4>
           </div>
 
-          <form>
+          <form @submit.prevent="submitHandler">
             <div class="input-field" >
-              <select>
-                <option>Category</option>
+              <select ref="select" v-model="current">
+                <option
+                v-for="c of categories"
+                :key="c.if"
+                :value="c.id"
+                >{{c.title}}</option>
               </select>
               <label>Выберите категорию</label>
             </div>
 
-            <div class="input-field">
-              <input type="text" id="name">
-              <label for="name">Название</label>
-              <span class="helper-text invalid">TITLE</span>
+         <div class="input-field">
+              <input
+                  id="name"
+                  type="text"
+                  v-model="title"
+                  :class="{invalid: $v.title.$dirty && !$v.title.required}"
+              >
+                <label for="name">Название</label>
+                <span 
+                class="helper-text invalid"
+                v-if="$v.title.$dirty && !$v.title.required"
+                >
+                Введите название категории
+              </span>
             </div>
 
             <div class="input-field">
               <input
                   id="limit"
                   type="number"
+                  v-model.number="limit"
+                  :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}"
+              
               >
               <label for="limit">Лимит</label>
-              <span class="helper-text invalid">LIMIT</span>
+              <span 
+              class="helper-text invalid"
+              v-if="$v.limit.$dirty && !$v.limit.minValue"
+              >Минимальное значение {{$v.limit.$params.minValue.min}}</span>
             </div>
 
             <button class="btn waves-effect waves-light" type="submit">
@@ -36,3 +56,67 @@
         </div>
       </div>
 </template>
+
+<script>
+import {required, minValue} from 'vuelidate/lib/validators'
+
+export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true
+    }
+  },
+  data: () => ({
+    select: null,
+    title: '',
+    limit: 100,
+    current: null
+  }),
+  watch:{
+    current(catId){
+      const{title, limit} = this.categories.find(c => c.id === catId)
+      this.title = title
+      this.limit = limit
+    }
+  },
+  created(){
+    const {id, title, limit} = this.categories[0]
+    this.current = id
+    this.title = title
+    this.limit = limit
+  },
+  mounted(){
+    this.select = M.FormSelect.init(this.$refs.select)
+    M.updateTextFields()
+  },
+  validations: {
+    title: {required},
+    limit: {minValue: minValue(100)}
+  },
+  methods:{
+    async submitHandler(){
+            if (this.$v.$invalid){
+                this.$v.$touch()
+                return
+            }
+            try{
+                const categoryData ={
+                  id: this.current,
+                  title: this.title,
+                  limit: this.limit
+                }
+                await this.$store.dispatch('updateCategory', categoryData)
+                this.$message('Категория обновлена')
+                this.$emit('updated', categoryData)
+            }catch(e){}
+            
+        }
+  },
+  destroyed() {
+    if(this.select && this.select.destroy){
+      this.select.destroy()
+    }
+  }
+}
+</script>
