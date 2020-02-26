b0317806286f61f08e49ca4df88928d4a30c4f56
Dan1eLx <d4n1elx3@gmail.com>
2020-02-27 01:28:21 +0500

    seventeenth lesson

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
+import Paginate from 'vuejs-paginate'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -23,6 +24,7 @@ Vue.filter('date', dateFilter);
 Vue.filter('currency', CurrencyFilter);
 Vue.directive('tooltip', tooltipDirective)
 Vue.component('Loader', Loader)
+Vue.component('Paginate', Paginate)
 
 firebase.initializeApp({
   apiKey: "AIzaSyAPw6OXND8Dfm9jWTgRdZ6XyxtFezvSAVE",
